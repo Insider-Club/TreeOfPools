11d82d562d16c5176401f9c46efc63e744369eae
JediFaust <omur.kubanychbekov@gmail.com>
2022-07-28 01:18:40 +0600

    fixed initial values

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-etherscan");
-const { PK, TBSC_API_KEY } = require("./env.json");
+// const { PK, TBSC_API_KEY } = require("./env.json");
 
 module.exports = {
   solidity: {
@@ -19,33 +19,36 @@ module.exports = {
     ],
   },
   networks: {
-    testbsc: {
-      url: `https://data-seed-prebsc-1-s3.binance.org:8545`,
-      accounts: [PK],
+    hardhat: {
       gas: 21000000,
-    },
-    eth: {
-      url: `https://eth-mainnet.public.blastapi.io`,
-      accounts: [PK],
-      gas: 21000000,
-    },
-    bsc: {
-      url: `https://bsc-dataseed2.binance.org`,
-      accounts: [PK],
-      gas: 21000000,
-    },
-    polygon: {
-      url: `https://polygon-mainnet.public.blastapi.io`,
-      accounts: [PK],
-      gas: 21000000,
-    },
-    moonbeam: {
-      url: `https://moonbeam.public.blastapi.io`,
-      accounts: [PK],
-      gas: 21000000,
-    },
+    }, 
+  //   testbsc: {
+  //     url: `https://data-seed-prebsc-1-s3.binance.org:8545`,
+  //     accounts: [PK],
+  //     gas: 21000000,
+  //   },
+  //   eth: {
+  //     url: `https://eth-mainnet.public.blastapi.io`,
+  //     accounts: [PK],
+  //     gas: 21000000,
+  //   },
+  //   bsc: {
+  //     url: `https://bsc-dataseed2.binance.org`,
+  //     accounts: [PK],
+  //     gas: 21000000,
+  //   },
+  //   polygon: {
+  //     url: `https://polygon-mainnet.public.blastapi.io`,
+  //     accounts: [PK],
+  //     gas: 21000000,
+  //   },
+  //   moonbeam: {
+  //     url: `https://moonbeam.public.blastapi.io`,
+  //     accounts: [PK],
+  //     gas: 21000000,
+  //   },
   },
-  etherscan: {
-    apiKey: TBSC_API_KEY,
-  },
+  // etherscan: {
+  //   apiKey: TBSC_API_KEY,
+  // },
 };
